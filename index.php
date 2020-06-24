9420883fabcbdc7ac1e53993ee7b71e3826bd30e
victorhcostas <victor99costa@outlook.com>
2020-06-24 15:48:42 -0300

    Site - Categories

--- index.php
+++ index.php
@@ -282,7 +282,7 @@ $app->get("/admin/categories/:idcategory/delete", function($idcategory) { //excl
 
 });
 
-$app->get("/admin/categories/:idcategory", function($idcategory) {
+$app->get("/admin/categories/:idcategory", function($idcategory) { //Exibe a tela de alteracao de dados de uma categoria
 
 	User::verifyLogin();
 
@@ -298,7 +298,7 @@ $app->get("/admin/categories/:idcategory", function($idcategory) {
 
 });
 
-$app->post("/admin/categories/:idcategory", function($idcategory) {
+$app->post("/admin/categories/:idcategory", function($idcategory) { //Altera os dados de uma categoria
 
 	User::verifyLogin();
 
@@ -312,6 +312,21 @@ $app->post("/admin/categories/:idcategory", function($idcategory) {
 
 	header('Location: /admin/categories');
 	exit;
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory) {
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>[]
+	]);
 
 });
 
