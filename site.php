9dac62b188b2f58d8284dec0f19407e5ade480bd
victorhcostas <victor99costa@outlook.com>
2020-07-03 15:16:00 -0300

    Integração PayPal HTML

--- site.php
+++ site.php
@@ -257,7 +257,18 @@ $app->post("/checkout", function() { //Realiza o checkout com verificacao de pre
 
 	$order->save();
 
-	header("Location: /order/".$order->getidorder() . "/pagseguro");
+	switch ((int)$_POST['payment-method']) {
+
+		case 1:
+			header("Location: /order/".$order->getidorder() . "/pagseguro");
+		break;
+
+		case 2:
+			header("Location: /order/".$order->getidorder() . "/paypal");
+		break;
+
+	}
+
 	exit;
 
 });
@@ -285,6 +296,29 @@ $app->get("/order/:idorder/pagseguro", function($idorder) {
 			'areaCode'=>substr($order->getnrphone(), 0, 2),
 			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
 		]
+	]);
+
+});
+
+$app->get("/order/:idorder/paypal", function($idorder) {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+	
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-paypal", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts(),
 	]);
 
 });
