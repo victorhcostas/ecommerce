ae8d1332f497ee809968a83bd1cc21cd62802bd4
victorhcostas <victor99costa@outlook.com>
2020-07-03 03:55:15 -0300

    Admin - Categorias - Busca e Paginacao

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -110,7 +110,7 @@ class Category extends Model {
 
     }
 
-    //Especifica o numero de produtos que aparecem numa pagina
+    //Especifica o numero de produtos que aparecem numa pagina da loja
     public function getProductsPage ($page = 1, $itemsPerPage = 2) {
 
         $start = ($page - 1) * $itemsPerPage;
@@ -132,6 +132,55 @@ class Category extends Model {
 
         return [
             'data'=>Product::checkList($results),
+            'total'=>(int)$resultTotal[0]["nrtotal"], 
+            'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //A funcao ceil arredonda numeros inteiros para cima
+        ];
+
+    }
+
+    //Especifica o numero de usuarios que aparecem numa pagina
+    public static function getPage ($page = 1, $itemsPerPage = 4) {
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_categories
+            ORDER BY descategory
+            LIMIT $start, $itemsPerPage;
+        ");
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>$results,
+            'total'=>(int)$resultTotal[0]["nrtotal"], 
+            'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //A funcao ceil arredonda numeros inteiros para cima
+        ];
+
+    }
+
+    //Faz uma busca e especifica o numero de usuarios encontrados que aparecem numa pagina
+    public static function getPageSearch ($search, $page = 1, $itemsPerPage = 4) {
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+            FROM tb_categories 
+            WHERE descategory LIKE :search
+            ORDER BY descategory
+            LIMIT $start, $itemsPerPage;
+        ", [
+            ':search'=>'%' . $search . '%'
+        ]);
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+            'data'=>$results,
             'total'=>(int)$resultTotal[0]["nrtotal"], 
             'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //A funcao ceil arredonda numeros inteiros para cima
         ];
